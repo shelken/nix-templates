cf0b6dcccd5bd7b648e1891ef77947a5d310aef5
shelken <shelken.pxk@gmail.com>
2026-04-26 05:35:42 +0800

    update base template

--- base/flake.nix
+++ base/flake.nix
@@ -1,35 +1,62 @@
 {
-  description = "Base development template";
+  description = "Development Template";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/25.05";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
-    nixpkgs,
-    utils,
-    ...
-  }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      devShells.default = pkgs.mkShell {
-        name = "base template";
-        
-        packages = with pkgs; [
-          
-        ];
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      git-hooks,
+      ...
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            end-of-file-fixer = {
+              enable = true;
+            };
+            trim-trailing-whitespace = {
+              enable = true;
+            };
+            check-merge-conflicts = {
+              enable = true;
+            };
+          };
+        };
+      in
+      {
+        checks.pre-commit-check = pre-commit-check;
+        devShells.default = pkgs.mkShell {
+          name = "base template";
 
-        shellHook = ''
-          echo "环境初始化成功"
-        '';
+          packages = with pkgs; [
+          ];
 
-        # Now we can execute any commands within the virtual environment.
-        # This is optional and can be left out to run pip manually.
-        postShellHook = ''
-          # allow pip to install wheels
-        '';
-      };
-    });
+          shellHook = ''
+            ${pre-commit-check.shellHook}
+          '';
+
+          # Now we can execute any commands within the virtual environment.
+          # This is optional and can be left out to run pip manually.
+          postShellHook = ''
+            # allow pip to install wheels
+          '';
+        };
+      }
+    );
 }
